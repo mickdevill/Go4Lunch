9e882135a1d28e8e37f6a47beccd65653b98c194
Mickdevil <trxctvolka@gmail.com>
2021-01-22 08:36:49 +0100

    instal some libs on project

--- app/src/main/java/com/mickdevil/go4lunch/MainActivity.java
+++ app/src/main/java/com/mickdevil/go4lunch/MainActivity.java
@@ -3,12 +3,31 @@ package com.mickdevil.go4lunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
+
+    String str = "sdfsfsfsfs";
+
+    TextView text;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+  text = findViewById(R.id.text);
+  btn = findViewById(R.id.btn);
+
     }
+
+
+
+
+
 }
