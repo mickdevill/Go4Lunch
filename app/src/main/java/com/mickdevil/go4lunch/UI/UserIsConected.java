ab4e62d496c7203f1fbd3d1c49e50b3779e6906f
Mickdevil <trxctvolka@gmail.com>
2021-01-23 07:00:13 +0100

    think about the multi dex, commit becose i begin the work with FB auth

---
+++ app/src/main/java/com/mickdevil/go4lunch/UI/UserIsConected.java
@@ -0,0 +1,16 @@
+package com.mickdevil.go4lunch.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mickdevil.go4lunch.R;
+
+public class UserIsConected extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_is_conected);
+    }
+}
